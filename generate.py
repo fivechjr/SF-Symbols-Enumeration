1f76e73eaeb9c4cd12977ef5bb28d6d418a8367f
Patcharapat Chaijaroen <32599524+fivechjr@users.noreply.github.com>
2020-07-25 15:07:50 +0700

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,38 @@
+# python3 generate.py >> SFSymbols.swift
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+import os
+import plistlib
+
+def capitalize(index, text):
+	out = text.capitalize() if index != 0 else text
+	if index == 0:
+		if text[0].isdigit():
+			out = '_' + out
+	return out
+
+def generateSymbolName(symbolName):
+	name = []
+	components = symbolName.split('.')
+	for index, component in enumerate(components):
+		name.append(capitalize(index, component))
+	return "".join(name)			
+
+def generateSwiftEnumeration(propertyList):
+	print("enum SFSymbols: String {")
+	for property in propertyList['symbols']:
+		print('    ' + generateSwiftEnumerationCase(property))
+	print("}")	
+
+def generateSwiftEnumerationCase(property):
+	return 'case ' + generateSymbolName(property) + ' = ' + '"' + property + '"'
+
+def main():
+	fileName = os.path.expanduser('NameAvailability.plist')
+	propertyList = plistlib.readPlist(fileName)
+	if 'symbols' in propertyList:
+		generateSwiftEnumeration(propertyList)
+
+main()
